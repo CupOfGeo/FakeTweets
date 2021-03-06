94c2b733720f25e5cc912ef34054a978c86f69c4
George <mazzeogeorge@gmail.com>
2021-03-06 08:27:54 -0600

    multipage works

--- Temp/apps/app1.py
+++ Temp/apps/app1.py
@@ -5,22 +5,6 @@ from dash.dependencies import Input, Output
 from app import app
 
 layout = html.Div([
-    html.H3('App 1'),
-    dcc.Dropdown(
-        id='app-1-dropdown',
-        options=[
-            {'label': 'App 1 - {}'.format(i), 'value': i} for i in [
-                'NYC', 'MTL', 'LA'
-            ]
-        ]
-    ),
-    html.Div(id='app-1-display-value'),
-    dcc.Link('Go to App 2', href='/apps/app2')
+    html.H3('Welcome!'),
+    dcc.Link('Go to App', href='Main')
 ])
-
-
-@app.callback(
-    Output('app-1-display-value', 'children'),
-    Input('app-1-dropdown', 'value'))
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
--- Temp/index.py
+++ Temp/index.py
@@ -4,6 +4,7 @@ from dash.dependencies import Input, Output
 
 from app import app
 from apps import app1, app2
+from apps import BetterDash
 
 
 app.layout = html.Div([
@@ -15,12 +16,12 @@ app.layout = html.Div([
 @app.callback(Output('page-content', 'children'),
               Input('url', 'pathname'))
 def display_page(pathname):
-    if pathname == '/apps/app1':
+    if pathname == '/Home':
         return app1.layout
-    elif pathname == '/apps/app2':
-        return app2.layout
+    elif pathname == '/Main':
+        return BetterDash.layout
     else:
-        return '404'
+        return app1.layout
 
 if __name__ == '__main__':
     app.run_server(debug=True)
